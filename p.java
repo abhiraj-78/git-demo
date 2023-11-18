647462c5cb08528df6d127f6f100c1276e539817
Abhimanyu <abhimanyurajput051@gmail.com>
2023-11-18 14:39:12 +0530

    program java

---
+++ p.java
@@ -0,0 +1,24 @@
+class ParkingLot{
+    int vno ;
+    int hours ;
+    double bill ;
+     
+    public void input(int vno ,int hours){
+        this.vno = vno ;
+        this.hours = hours ;
+    }
+        void display(){
+            if(hours == 1){
+                System.out.println(hours*);
+            }
+            else if(hours > 1){
+
+            }
+        }
+}
+class Main{
+    public static void main(String[] args) {
+        
+
+    }
+}
